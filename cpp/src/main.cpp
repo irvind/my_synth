bec5971f89a90afdfc020c54a4dcc6e2eb74d3a6
Eugene Obukhov <irvind25@gmail.com>
2023-07-13 03:16:36 +0300

    Figuring out how unsigned/signed works

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include "yswavfile.h"
 
 int toSigned16Bit(unsigned char leastByte, unsigned char mostByte)
@@ -22,6 +23,29 @@ void printWavDataPositions(YsWavFile *wavFile, int posCount)
         );
 }
 
+void printUnsignedSignedValues()
+{
+    unsigned char chars[6] = {255, 254, 128, 127, 126, 0};
+    for (int i = 0; i < 6; i++)
+        printf("signed: %d / unsigned: %u\n", (char)chars[i], chars[i]);
+
+    unsigned char ch1 = 255;
+    char ch2 = ch1;
+    unsigned char ch3 = ch2; 
+    printf("signed ch1 d: %d / unsigned ch2 d: %d\n", ch1, ch2);
+    printf("unsigned ch3 d: %d\n", ch3);
+
+    unsigned char ch4 = 128;
+    int num = ch4;  
+    printf("num - %d\n", num);
+
+    unsigned char ch5 = 128;
+    char ch6 = ch5;
+    int num2 = ch6;
+    // printf("ch6 - %d\n", ch6);
+    printf("num2 - %d\n", num2);
+}
+
 int main(int argc, char* argv[])
 {
     if (argc < 2)
@@ -41,5 +65,7 @@ int main(int argc, char* argv[])
     std::cout << "WAV file has been parsed" << std::endl;
 
     printWavDataPositions(&wavFile, 500);
+    // printUnsignedSignedValues();
+
     return 0;
 }
